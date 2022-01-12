333eed1144d81786322eb2dd776d6016b6d5a9ad
Pierre-Julien VILLOUD <pjvilloud@protonmail.com>
2022-01-12 11:35:43 +0100

    Exercice 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    /**
-     *
-     * @param args
-     */
+    public static final short MAX_PTS_VIE = 100;
+    public static final short PTS_BOUCLIER = 25;
+    public static final short MAX_ATTAQUE_ENNEMI = 5;
+    public static final short MAX_VIE_ENNEMI = 30;
+    public static final short MAX_ATTAQUE_JOUEUR = 5;
+    public static final short REGENARATION_BOUCLIER_PAR_TOUR = 10;
+
 }
